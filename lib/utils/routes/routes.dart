06cd80e897db8474120723a0eaefd3ccb1b77b9e
Akshay <akshaypatole338@gmail.com>
2023-12-11 19:12:19 +0530

    splash screen completed

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:baliraja/presentatioin/cow_sales_screen/cow_sales_screen.dart';
 import 'package:baliraja/presentatioin/edit_profile_screen/edit_profile_screen.dart';
+import 'package:baliraja/presentatioin/splash_screen/splash_screen.dart';
 import 'package:baliraja/presentatioin/user_account_screen/user_account_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,10 +13,10 @@ class Routes {
   late final GoRouter _goRouter = GoRouter(
     initialLocation: splashScreen,
     routes: [
-      // GoRoute(
-      //   path: splashScreen,
-      //   builder: (context, state) => SplashScreen(),
-      // ),
+      GoRoute(
+        path: splashScreen,
+        builder: (context, state) => const SplashScreen(),
+      ),
       GoRoute(
         path: useraccountscreen,
         builder: (context, state) => const UserAccountScreen(),
